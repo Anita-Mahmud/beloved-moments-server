0b6d1dbeadc207750ab59262594675f6e7e05732
Anita-Mahmud <anita.mahmud13@gmail.com>
2022-11-10 01:16:43 +0600

    middlewares added

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const port = process.env.PORT|| 5000;
+
 const app = express();
-app.get('/',(req,res)=>{
-    res.send('Server running');
-});
-app.listen(port,()=>{
-    console.log('Server running on port',port);
-});
+const port = process.env.PORT || 5000;
+
+// middle wares
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Server is running')
+})
+
+app.listen(port, () => {
+    console.log(`Server running on ${port}`);
+})
