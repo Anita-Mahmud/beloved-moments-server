1b7324675375cedfc40a986312ecf126d17549fb
Anita-Mahmud <anita.mahmud13@gmail.com>
2022-11-10 15:36:26 +0600

    all services added

--- index.js
+++ index.js
@@ -10,13 +10,32 @@ app.use(cors());
 app.use(express.json());
 
 //MONGODB
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qspwkqa.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+// console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+      const db = client.db("belovedMoments");
+      const servicesCollection = db.collection("services");
+      app.get('/services', async (req, res) => {
+        const query = {};
+        const cursor = servicesCollection.find(query);
+        const services = await cursor.limit(3).toArray();
+        res.send(services);
+    });
+    app.get('/all/services', async (req, res) => {
+        const query = {};
+        const cursor = servicesCollection.find(query);
+        const all_services = await cursor.toArray();
+        res.send(all_services);
+    });
 
+    }
+    finally{}
+}
+run().catch(er=>console.log(err));
+      
 
 app.get('/', (req, res) => {
     res.send('Server is running')
